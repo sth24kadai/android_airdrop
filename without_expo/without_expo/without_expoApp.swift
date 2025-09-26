4b7c34cd202268186b4f2e2f54c419ae2b758fa7
ao132AO <136077279+ao132AO@users.noreply.github.com>
2025-09-26 17:38:53 +0900

    swift uiでビルド成功

---
+++ without_expo/without_expo/without_expoApp.swift
@@ -0,0 +1,17 @@
+//
+//  without_expoApp.swift
+//  without_expo
+//
+//  Created by YO AK on 2025/09/25.
+//
+
+import SwiftUI
+
+@main
+struct without_expoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
