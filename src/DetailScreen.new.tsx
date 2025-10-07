2b99c25c21c5c28e89460117039491a2cc7f4a1d
ao132AO <136077279+ao132AO@users.noreply.github.com>
2025-10-07 09:21:11 +0900

    gfeirgeurghergud

--- src/DetailScreen.new.tsx
+++ src/DetailScreen.new.tsx
@@ -1,82 +1,100 @@
-
-import 'react-native-gesture-handler'
-import { ContextType } from "react"
-import { 
+import "react-native-gesture-handler";
+import { ContextType } from "react";
+import {
     StyleSheet,
     View,
     ScrollView as RNScrollView,
-    GestureResponderEvent,
-} from 'react-native'
-import {
-    Button,
-    Text as PaperText 
-} from "react-native-paper"
-import { Context } from '../components/context'
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
-import { AutoHeightImage } from '../components/autosizedImage'
-import { NetworkInfo } from 'react-native-network-info'
-import { ShardSender } from '../components/shardSender'
-import { HTTPBufferRequest } from '../types'
-import { Service } from 'react-native-zeroconf'
+    Text,
+} from "react-native";
+import { Button, Text as PaperText } from "react-native-paper";
+import { Context } from "../components/context";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { AutoHeightImage } from "../components/autosizedImage";
+import { NetworkInfo } from "react-native-network-info";
+import { ShardSender } from "../components/shardSender";
+import { HTTPBufferRequest, RootStackParamList } from "../types";
+import { Service } from "react-native-zeroconf";
 
-
-export default class DetailScreen extends ShardSender<'DetailScreen'> {
+export default class DetailScreen extends ShardSender<"DetailScreen"> {
     /**
      * Context
      */
-    static contextType : typeof Context = Context
-	//@ts-ignore - エラー回避の方法が思いつかない。抽象にしても宣言にしてもすべてエラーではじかれるから萎えた。
-    context !: ContextType< typeof Context >
-    
-    public readonly HTTP_PORT : number = 8771
-    public readonly BYTES : number = 32768 * 2 // 64KB
+    static contextType: typeof Context = Context;
+    //@ts-ignore - エラー回避の方法が思いつかない。抽象にしても宣言にしてもすべてエラーではじかれるから萎えた。
+    context!: ContextType<typeof Context>;
+
+    public readonly HTTP_PORT: number = 8771;
+    public readonly BYTES: number = 32768 * 2; // 64KB
     public state = {
-        isSending : false,
-        startTime : 0,
-        ip : ""
-    }
+        isSending: false,
+        startTime: 0,
+        ip: "",
+        estimate: 0,
+    };
+
+    public timer: NodeJS.Timeout | null = null;
 
     componentDidMount(): void {
-        NetworkInfo.getIPV4Address().then(v => {
-			this.setState({
-				ip: v
-			}) // 自身の追跡用にIPを決定させておく
-		})
+        NetworkInfo.getIPV4Address().then((v) => {
+            this.setState({
+                ip: v,
+            }); // 自身の追跡用にIPを決定させておく
+        });
+
+        this.timer = setInterval(() => {
+            if (this.state.isSending) {
+                if (this.estimate !== this.state.estimate) {
+                    this.setState({
+                        estimate: this.estimate,
+                    });
+                }
+            }
+        }, 500);
     }
 
-    callback( sentShards : HTTPBufferRequest[] ) {
+    componentWillUnmount(): void {
+        if (this.timer) {
+            clearInterval(this.timer);
+            this.timer = null;
+        }
+    }
+
+    callback(sentShards: HTTPBufferRequest[]) {
         this.context.setObjectState({
             image: null,
-            sentShards: [ ...this.context.sentShards, ...sentShards],
-        })
-        this.props.navigation.navigate('SelectImageInitScreen')
+            sentShards: [...this.context.sentShards, ...sentShards],
+        });
+        this.props.navigation.navigate("SelectImageInitScreen");
     }
 
-    onButtonPress( service: Service & { clientName : string, clientModel : string } ) {
-
-        if( service === null ){
-            this.context.logs.push({ emoji: "🤬", message: "サービスが選択されていません。" })
+    onButtonPress(
+        service: Service & { clientName: string; clientModel: string }
+    ) {
+        if (service === null) {
+            this.context.logs.push({
+                emoji: "🤬",
+                message: "サービスが選択されていません。",
+            });
             return;
         }
 
         this.setState({
-            isSending: true
-        })
+            isSending: true,
+        });
 
         this.sendImage(
             service,
             this.context.image,
             this.context.selectedService,
-            ( sentShards ) => this.callback( sentShards )
-        )
+            (sentShards) => this.callback(sentShards)
+        );
     }
 
     render() {
-
         const { services, selectedService } = this.context;
         const service = selectedService ? services[selectedService] : null;
 
-        if( service === null ){
+        if (service === null) {
             return (
                 <SafeAreaProvider>
                     <SafeAreaView>
@@ -84,46 +102,72 @@ export default class DetailScreen extends ShardSender<'DetailScreen'> {
                         <PaperText> サービスが選択されていません </PaperText>
                     </SafeAreaView>
                 </SafeAreaProvider>
-            )
+            );
         }
 
         return (
             <SafeAreaProvider>
-                <SafeAreaView style={ styles.container }>
+                <SafeAreaView style={styles.container}>
                     <RNScrollView>
-                        <Button 
-                            style={styles.upMargin} 
-                            mode="contained" 
-                            onPress={() => this.onButtonPress( service )} 
-                            disabled={this.state.isSending || !this.context.image} 
+                        <Button
+                            style={styles.upMargin}
+                            mode="contained"
+                            onPress={() => this.onButtonPress(service)}
+                            disabled={
+                                this.state.isSending || !this.context.image
+                            }
                         >
                             データを送信する
                         </Button>
                         <View style={styles.flexCenter}>
-                            {
-                                this.context.image && [
-                                    ...( Array.isArray( this.context.image ) ? this.context.image : [this.context.image])
-                                ].map(
-                                    (uri, index) => (
-                                        <AutoHeightImage 
-                                            style={styles.imageStyle} 
-                                            key={index} 
-                                            source={{ uri: uri }} 
-                                            width={350} 
+                            {this.context.image &&
+                                [
+                                    ...(Array.isArray(this.context.image)
+                                        ? this.context.image
+                                        : [this.context.image]),
+                                ].map((uri, index) =>
+                                    uri.isFile ? (
+                                        <Text> File </Text>
+                                    ) : (
+                                        <AutoHeightImage
+                                            style={styles.imageStyle}
+                                            key={index}
+                                            source={{ uri: uri.uri }}
+                                            width={350}
                                             hiddenDeleteBtn={true}
-                                        /> 
+                                        />
                                     )
-                                )
-                            }
+                                )}
                         </View>
+                        {this.state.isSending && (
+                            <View style={styles.statusComponent}>
+                                <PaperText>送信中...</PaperText>
+                                <PaperText>
+                                    推定残り時間:{" "}
+                                    {Math.round(this.state.estimate / 1000)}秒
+                                </PaperText>
+                            </View>
+                        )}
                     </RNScrollView>
                 </SafeAreaView>
             </SafeAreaProvider>
-        )
+        );
     }
 }
 
 const styles = StyleSheet.create({
+    statusComponent: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginTop: 10,
+        marginBottom: 10,
+        borderRadius: 13,
+        borderStyle: "solid",
+        borderWidth: 2,
+        borderColor: "#e3e3e3",
+        padding: 10,
+    },
     upMargin: {
         marginTop: 10,
     },
@@ -134,29 +178,29 @@ const styles = StyleSheet.create({
         borderColor: "#e3e3e3",
     },
     container: {
-		flex: 1,
-		marginLeft: 10,
-		marginRight: 10,
-		marginBottom: 10,
+        flex: 1,
+        marginLeft: 10,
+        marginRight: 10,
+        marginBottom: 10,
         marginTop: 10,
         borderStyle: "solid",
         borderWidth: 1,
         borderColor: "#f0f0f0",
         minHeight: 100,
-        minWidth: "100%"
-	},
-    flexCenter : {
-        flex : 1,
-        flexDirection : "column",
-        justifyContent : "center",
-        alignItems : "center",
-        marginTop : 10,
-        marginBottom : 10,
+        minWidth: "100%",
+    },
+    flexCenter: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 10,
+        marginBottom: 10,
     },
     udpadding: {
-		paddingTop: 10,
-		paddingBottom: 10
-	},
-})
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
+});
 
-DetailScreen.contextType = Context
+DetailScreen.contextType = Context;
--- src/ShowComingDatas.tsx
+++ src/ShowComingDatas.tsx
@@ -2,90 +2,99 @@ import { Component } from "react";
 import { NativeStackScreenProps } from "react-native-screens/lib/typescript/native-stack/types";
 import { RootStackParamList } from "../types";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { ScrollView, StyleSheet, View } from "react-native"
+import { Image, ScrollView, StyleSheet, View } from "react-native";
 import { Context } from "../components/context";
+import { Buffer } from "buffer";
 import { Button, Text } from "react-native-paper";
 import { AutoHeightImage } from "../components/autosizedImage";
 import RNFS from "react-native-fs";
-import { Notifier, Easing } from 'react-native-notifier';
-import { getFileTypeFromBuffer } from "../components/getFileTypeFromBuffer";
+import { Notifier, Easing } from "react-native-notifier";
 
-
-
-export default class ShowComingDatas extends Component<NativeStackScreenProps<RootStackParamList, '写真の保存'>> {
-
+export default class ShowComingDatas extends Component<
+    NativeStackScreenProps<RootStackParamList, "写真の保存">
+> {
     static contextType = Context;
     //@ts-ignore
-    context!: React.ContextType<typeof Context>
+    context!: React.ContextType<typeof Context>;
 
-    getRecentData(): { uri: string, name: string }[] {
+    getRecentData(): string[] {
         const { recivedDatas } = this.context;
         if (recivedDatas.length === 0) {
             return [];
         }
-        return recivedDatas.map((data) => ({ uri: data.uri, name: data.name }));
+        return recivedDatas.map((data) => data.uri);
     }
 
-    public getFileTypeFromBuffer(buffer: Uint8Array): string | null {
-        return getFileTypeFromBuffer( buffer )
+    getBase64URI(): string[] {
+        return this.getRecentData().filter((uri) =>
+            uri.startsWith("data:image/")
+        );
     }
 
-    saveImage(name: string, base64: string) {
+    saveImage(base64: string) {
         const base64URI = base64;
         if (base64URI.length === 0) {
             return;
         }
 
-        const path = RNFS.DownloadDirectoryPath + `/quickshare_images/${name}.${base64URI.split(';')[0].split('/')[1]}`;
-        RNFS.mkdir(RNFS.DownloadDirectoryPath + "/quickshare_images").catch(() => { });
-        // 保存
-        RNFS.writeFile(path, base64URI.split(',')[1], 'base64')
+        const path =
+            RNFS.DownloadDirectoryPath +
+            `/quickshare.${Date.now()}.${
+                base64URI.split(";")[0].split("/")[1]
+            }`;
+        RNFS.writeFile(path, base64URI.split(",")[1], "base64")
             .then(() => {
                 Notifier.showNotification({
-                    title: '保存しました',
-                    description: '画像をダウンロードフォルダ内に保存しました。',
+                    title: "保存しました",
+                    description: "画像をダウンロードフォルダ内に保存しました。",
                     duration: 5000,
                     showAnimationDuration: 800,
                     showEasing: Easing.inOut(Easing.ease),
-                    onHidden: () => console.log('hidden'),
-                    onPress: () => console.log('press'),
+                    onHidden: () => console.log("hidden"),
+                    onPress: () => console.log("press"),
                     hideOnPress: true,
-                })
+                });
             })
             .catch((err) => {
                 console.log(err.message);
             });
     }
 
-
-
     render() {
         return (
             <SafeAreaProvider>
                 <SafeAreaView style={styles.baseFlexStyle}>
                     <ScrollView>
-                        {
-                            this.getRecentData().length > 0 ? (
-                                this.getRecentData().map((data, index) => (
-                                    <View key={index} style={styles.containText}>
-                                        {data.uri.startsWith('data:image/') ? (
-                                            <AutoHeightImage style={styles.imageStyle} width={350} source={{ uri: data.uri }} hiddenDeleteBtn />
-                                        ) : (
-                                            <Text> その他画像以外のファイル ({data.uri.split(',')[0]}) {data.name}</Text>
-                                        )}
-                                        <Button mode="contained-tonal" onPress={() => this.saveImage(data.name, data.uri)} >
-                                            保存する
-                                        </Button>
-                                    </View>
-                                ))
-                            ) : (
-                                <Text style={styles.noImageText}>最近シェアされた写真はまだないようです。</Text>
-                            )
-                        }
+                        {this.getRecentData().length > 0 ? (
+                            this.getRecentData().map((uri, index) => (
+                                <View key={index} style={styles.containText}>
+                                    {uri.startsWith("data:image/") ? (
+                                        <AutoHeightImage
+                                            style={styles.imageStyle}
+                                            width={350}
+                                            source={{ uri: uri }}
+                                            hiddenDeleteBtn
+                                        />
+                                    ) : (
+                                        <div> データファイル </div>
+                                    )}
+                                    <Button
+                                        mode="contained-tonal"
+                                        onPress={() => this.saveImage(uri)}
+                                    >
+                                        保存する2
+                                    </Button>
+                                </View>
+                            ))
+                        ) : (
+                            <Text style={styles.noImageText}>
+                                最近シェアされた写真はまだないようです。
+                            </Text>
+                        )}
                     </ScrollView>
                 </SafeAreaView>
             </SafeAreaProvider>
-        )
+        );
     }
 }
 
@@ -97,14 +106,14 @@ const styles = StyleSheet.create({
         borderColor: "#e3e3e3",
     },
     baseFlexStyle: {
-        display: 'flex',
-        height: '100%',
-        width: '100%',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignContent: 'center',
-        textAlign: 'center',
+        display: "flex",
+        height: "100%",
+        width: "100%",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        alignContent: "center",
+        textAlign: "center",
     },
     containText: {
         display: "flex",
@@ -115,11 +124,11 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         marginTop: 5,
         marginBottom: 5,
-        borderColor: "#e3e3e3"
+        borderColor: "#e3e3e3",
     },
     noImageText: {
-        display: 'flex',
-        justifyContent: 'center',
-        textAlign: 'center',
-    }
-})
+        display: "flex",
+        justifyContent: "center",
+        textAlign: "center",
+    },
+});
